74971f0fee0be4684f52c9369996534dd09d8459
zufarathoya <zufarathoyabahar@mail.ugm.ac.id>
2024-10-29 15:26:12 +0700

    after 2nd presentation

--- My Pacman/Pacman.py
+++ My Pacman/Pacman.py
@@ -16,47 +16,58 @@ tournament_entrant_count = 2      # how many entree in a tournament
 
 
 # one loop
-
 populations = create_init_population_bin(game_length, pop_count)
-pop_fitnesses = [random.random() for x in range(len(populations))]
-mating_pool = [populations[random.randint(0, len(populations) - 1)] for i in range(mating_pool_size)]
 
-children = []
-for i in range(mating_times):
-    parent1 = mating_pool[random.randint(0, len(mating_pool) - 1)]
-    parent2 = mating_pool[random.randint(0, len(mating_pool) - 1)]
-    child1, child2 = crossover(parent1, parent2)
-    children.append(child1)
-    children.append(child2)
+def pacman_ga(populations):
+    pop_fitnesses = [random.random() for x in range(len(populations))]
+    mating_pool = [populations[random.randint(0, len(populations) - 1)] for i in range(mating_pool_size)]
 
+    children = []
+    for i in range(mating_times):
 
-for i in range(len(children)):
-    children[i] = mutation(children[i], mutation_probability)
+        # Pilih parent pake tournament
+        parent1 = mating_pool[random.randint(0, len(mating_pool) - 1)]
+        parent2 = mating_pool[random.randint(0, len(mating_pool) - 1)]
+        child1, child2 = crossover(parent1, parent2)
+        children.append(child1)
+        children.append(child2)
 
-populations += children
+    for i in range(len(children)):
+        children[i] = mutation(children[i], mutation_probability)
 
-pop_fitnesses = []
-for i in range(len(populations)):
-    game = Game()
-    inputs = chromosome_bin_iterator(populations[i])
-    game.injectInput(inputs, input_delay)
-    game.run()
+    populations += children
 
-    points = game.pacman.points
-    time_left = 0 if game.pacman.lives == 0 else game_length - game.frameElapsed // input_delay
-    fitness = calculate_fitness(points, time_left)
-    pop_fitnesses.append(fitness)
-    print(f'individual {i}: fitness = {fitness}')
+    pop_fitnesses = []
+    for i in range(len(populations)):
+        game = Game()
+        inputs = chromosome_bin_iterator(populations[i])
+        game.injectInput(inputs, input_delay)
+        game.run()
 
-new_populations = []
-for i in range(pop_count):
-    entries = []
-    for k in range(tournament_entrant_count):
-        l = random.randint(0, len(populations) - 1)
-        entries.append((populations[l], pop_fitnesses[l]))
-    winner = tournament(entries)
-    new_populations.append(winner)
+        points = game.pacman.points
+        time_left = 0 if game.pacman.lives == 0 else game_length - game.frameElapsed // input_delay
+        fitness = calculate_fitness(points, time_left)
+        pop_fitnesses.append(fitness)
+        print(f'individual {i+1}: fitness = {fitness}')
+    
+    # sort dan pilih n terbaik
+    new_populations = []
+    for i in range(pop_count):
+        entries = []
+        for k in range(tournament_entrant_count):
+            l = random.randint(0, len(populations) - 1)
+            entries.append((populations[l], pop_fitnesses[l]))
+        winner = tournament(entries)
+        new_populations.append(winner)
 
-populations = new_populations
+    populations = new_populations
+    # print(len(populations))
+
+    return populations
+
+for i in range(10):
+    print(f'generation {i+1}')
+    populations = pacman_ga(populations)
+    print('')
 
 # end loop
